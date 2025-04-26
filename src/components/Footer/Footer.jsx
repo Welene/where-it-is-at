adfa7613656bfd8a353a6f353b263bc62d6d3ded
Welene <helenetheo@live.no>
2025-04-26 20:01:48 +0200

    Halfway finished with SwipeNav in Footer // needs correct styling + routing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,4 @@
-import React from 'react';
-
-import Lottie from 'lottie-react';
-import animationData from './assets/loadingAnimation.json'; 
-// LOADING ANIMASJON TIL SENERE
-
+import './footer.css';
 // IMPORTERE SwipeBar greier her
 import SwipeNav from '../SwipeNavigation/SwipeNavigation';
 
@@ -12,6 +7,7 @@ function Footer() {
   return (
     <section className="footer-section">
         {/* lese inn SwipeNav komponenten her */}
+       <SwipeNav/> 
     </section>
   );
 }
--- src/components/SwipeNavigation/SwipeNavigation.jsx
+++ src/components/SwipeNavigation/SwipeNavigation.jsx
@@ -1,10 +1,37 @@
-function SwipeNav() {
+// SwipeNav.jsx
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
 
-    return (
-      <section className="footer-section">
-          {/* lese inn SwipeNav komponenten her */}
-      </section>
-    );
-  }
-  
-  export default SwipeNav;
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import './swipeNavigation.css';
+
+function SwipeNav() {
+return (
+    <section className="swipe-section">
+        <Swiper
+        // install modules
+        modules={[Navigation, Pagination, Scrollbar]}
+        spaceBetween={10}
+        slidesPerView={1}
+        direction="horizontal"
+        loop={true}
+        pagination={{ clickable: true }}
+        // navigation={true}
+        // scrollbar={{ draggable: true }}
+        className="swipe-section__navigation"
+        >
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        {/* Add more slides as needed */}
+      </Swiper>
+    </section>
+  );
+}
+
+export default SwipeNav;
