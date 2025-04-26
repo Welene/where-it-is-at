f2f8b7dc857ad3840f8126e580062925000218dc
Welene <helenetheo@live.no>
2025-04-26 13:01:27 +0200

    Added Footer element in Footer component // started on SwipeNav component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Lottie from 'lottie-react';
+import animationData from './assets/loadingAnimation.json'; 
+// LOADING ANIMASJON TIL SENERE
+
+// IMPORTERE SwipeBar greier her
+import SwipeNav from '../SwipeNavigation/SwipeNavigation';
+
+function Footer() {
+
+  return (
+    <section className="footer-section">
+        {/* lese inn SwipeNav komponenten her */}
+    </section>
+  );
+}
+
+export default Footer;
---
+++ src/components/SwipeNavigation/SwipeNavigation.jsx
@@ -0,0 +1,10 @@
+function SwipeNav() {
+
+    return (
+      <section className="footer-section">
+          {/* lese inn SwipeNav komponenten her */}
+      </section>
+    );
+  }
+  
+  export default SwipeNav;
