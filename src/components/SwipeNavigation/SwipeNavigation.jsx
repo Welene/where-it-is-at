e7ebdc116b708d24d99350202d3d5441368b4081
Welene <helenetheo@live.no>
2025-05-05 15:43:31 +0200

    Fixed gradient on ConfirmationPage (Tickets)

--- src/components/SwipeNavigation/SwipeNavigation.jsx
+++ src/components/SwipeNavigation/SwipeNavigation.jsx
@@ -14,33 +14,32 @@ import EventsPage from '../../pages/EventsPage/EventsPage';
 import ConfirmationPage from '../../pages/ConfirmationPage/ConfirmationPage';
 
 function SwipeNav() {
-return (
-    <section className="swipe-section">
-        <Swiper
-        // install modules
-        modules={[Navigation, Pagination, Scrollbar]}
-        // spaceBetween={0}
-        slidesPerView={1}
-        direction="horizontal"
-        loop={true}
-        pagination={{ clickable: true }}
-        // navigation={true}
-        // scrollbar={{ draggable: true }}
-        className="swipe-section__navigation"
-        >
-        <SwiperSlide>
-          <HomePage/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <EventsPage/> 
-        </SwiperSlide>
-        <SwiperSlide>
-          <ConfirmationPage/>
-        </SwiperSlide>
-        {/* Add more slides as needed */}
-      </Swiper>
-    </section>
-  );
+	return (
+		<section className="swipe-section">
+			<Swiper
+				// install modules
+				modules={[Navigation, Pagination, Scrollbar]}
+				// spaceBetween={0}
+				slidesPerView={1}
+				direction="horizontal"
+				loop={true}
+				pagination={{ clickable: true }}
+				// navigation={true}
+				// scrollbar={{ draggable: true }}
+				className="swipe-section__navigation">
+				<SwiperSlide>
+					<HomePage />
+				</SwiperSlide>
+				<SwiperSlide>
+					<EventsPage />
+				</SwiperSlide>
+				<SwiperSlide>
+					<ConfirmationPage />
+				</SwiperSlide>
+				{/* Add more slides as needed */}
+			</Swiper>
+		</section>
+	);
 }
 
 export default SwipeNav;
